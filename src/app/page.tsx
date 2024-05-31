ef3d4551aa228def7666cba4bb5489470dd99f18
zeptozor <zeptozor@gmail.com>
2024-05-31 20:35:39 +0500

    sigma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <div className="w-full h-screen flex items-center justify-center">Hello world</div>
+    return <div className="w-full h-screen flex items-center justify-center">Emm, what a sigma</div>
 }
